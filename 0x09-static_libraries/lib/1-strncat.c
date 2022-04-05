30c11770dfa00e1892faf7b60b7061cf5b646984
Niqy <niqynicole57@gmail.com>
2022-04-05 15:01:48 -0700

    Create static library libmy.a

--- 0x09-static_libraries/lib/1-strncat.c
+++ 0x09-static_libraries/lib/1-strncat.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
-char *_memcpy(char *dest __attribute__((unused)), char *src __attribute__((unused)), unsigned int n __attribute__((unused)))
+/**
+ * _strncat - function
+ *@dest: char
+ *@src: char
+ *@n: int
+ *Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
 {
-	return (0);
-}
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}}
--- 0x09-static_libraries/lib/2-strchr.c
+++ 0x09-static_libraries/lib/2-strchr.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
-char *_strchr(char *s __attribute__((unused)), char c __attribute__((unused)))
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to spot in s with c or null
+ */
+char *_strchr(char *s, char c)
 {
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
 	return (0);
 }
+
