d8f7cf932691e792d6f5f4bc82ec875688739f0b
Niqy <niqynicole57@gmail.com>
2022-04-11 13:50:12 -0700

    Add file 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_DEF
+#define ABS_DEF
 
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif /* end ABS_DEF */
