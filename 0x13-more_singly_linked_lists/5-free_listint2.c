7b5a827122b1142761c6a4934f7cc500be4d11a8
Niqy <niqynicole57@gmail.com>
2022-04-26 16:01:54 -0700

    Add file 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_listint2 - frees a listint_t list
@@ -22,5 +23,3 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(current);
 	}
-
-}
